d1966d4dd71427733e39cb75ce13a1fe62da68c9
Nick Lange <nick.lange@gmail.com>
2026-01-05 09:44:27 -0500

    Configure for Render: Restore storage.yml and configure Active Storage

--- web/config/environments/production.rb
+++ web/config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :render
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
